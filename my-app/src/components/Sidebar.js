e1f9768309d25b8a0f29adbe9dad191c45299cae
JamieB92 <j.bonner6992@gmail.com>
2023-10-10 20:49:39 +0000

    complete first react challange

---
+++ my-app/src/components/Sidebar.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+
+function Sidebar() {
+    return <div><a href="/">MyPhotos</a> <a href="/">My Illustrations</a> <a href="/">My Paintins</a></div>
+}
+
+export default Sidebar;
