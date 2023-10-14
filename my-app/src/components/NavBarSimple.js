5579969b62d68d0e2ae18840ba246ee5a1cd2112
JamieB92 <j.bonner6992@gmail.com>
2023-10-14 16:38:46 +0000

    create a nav bar for React challange

---
+++ my-app/src/components/NavBarSimple.js
@@ -0,0 +1,36 @@
+import React from "react"
+import css from "./css/NavBarSimple.module.css";
+
+class NavBarSimple extends React.Component{
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            message: "Hello, guest!",
+            buttonText: "Login"
+        }
+    }
+
+    handleClick = () => {
+        this.setState((prevState) => ({
+            message: prevState.message === "Hello, guest!" ? "Welcome back, user!" : "Hello, guest!",
+            buttonText: prevState.buttonText === "Log out" ? "Login" : "Log out",
+        }), ()=> console.log(this.state.message))
+    }
+
+
+    render() {
+        return(
+            <div className={css.NavBar}>
+                <h1>My Gallery</h1>
+                <div>
+                    <span>{this.state.message}</span>
+                    <button onClick={() => this.handleClick()}>{this.state.buttonText}</button>
+                </div>
+            </div>
+
+        )
+    }
+}
+
+export default NavBarSimple
