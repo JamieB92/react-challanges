93dc790294f66ab5556485c1d6632a0e3db3a938
JamieB92 <j.bonner6992@gmail.com>
2023-10-16 20:21:29 +0000

    create navbar form and also re factor to a nested child component

--- my-app/src/components/Sidebar.js
+++ my-app/src/components/Sidebar.js
@@ -4,7 +4,7 @@ import css from "./css/Sidebar.module.css"
 
 function Sidebar() {
     return (
-        <div className={css.sidebar} >
+        <div className={css.Sidebar} >
             <a href="/"> MyPhotos</a>
             <a href="/"> My Illustrations</a>
             <a href="/"> My Paintings</a>
